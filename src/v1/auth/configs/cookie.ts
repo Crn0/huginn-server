f5691a54761f72407a0700ad981d600904c23bd8
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-10 14:54:01 +0800

    refactor(auth/configs/cookie): set path=/api

--- src/v1/auth/configs/cookie.ts
+++ src/v1/auth/configs/cookie.ts
@@ -1,6 +1,7 @@
 import { env } from "@/configs/env.js";
 
 export const cookieConfig = {
+  path: "/api",
   httpOnly: true,
   secure: env.NODE_ENV === "prod",
   sameSite: env.NODE_ENV === "prod" ? "none" : "lax",
