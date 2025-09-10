626151f34f3cbb1466d5a8c3c361246a004c4f6b
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-10 16:08:31 +0800

    chore(lib/user-schema.ts): update reference link for username regex

--- src/v1/lib/user-schema.ts
+++ src/v1/lib/user-schema.ts
@@ -17,7 +17,7 @@ const profileMediaSchema = z.object({
   }),
 });
 
-// https://regexr.com/8dmei
+// https://regexr.com/8h173
 export const usernameRegex = /^[a-zA-Z0-9{_,.}]+$/;
 // https://regexr.com/8dm04
 export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
