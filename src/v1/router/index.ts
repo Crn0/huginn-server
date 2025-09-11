56e7c477f4c228f012f23fee3c752540749e87f0
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-11 17:38:57 +0800

    feat(router): register user routes in main router

--- src/v1/router/index.ts
+++ src/v1/router/index.ts
@@ -3,11 +3,13 @@ import { rateLimit } from "express-rate-limit";
 
 import { rateLimitOptions } from "../configs/rate-limiter.js";
 import { router as authRouter } from "../auth/routes/index.js";
+import { router as userRouter } from "../user/routes/index.js";
 
 const router = Router();
 
 router.use(rateLimit(rateLimitOptions));
 
 router.use("/api/v1/auth", authRouter);
+router.use("/api/v1/users", userRouter);
 
 export { router };
