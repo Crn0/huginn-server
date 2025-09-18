0962581656ceaefcf884e03e33cbbc281c33bc5b
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-18 15:22:29 +0800

    refactor(lib/validator): clean up files on error

--- src/v1/lib/validator.ts
+++ src/v1/lib/validator.ts
@@ -3,10 +3,35 @@ import z from "zod";
 import { ValidationError } from "@/lib/errors/validation-error.js";
 
 import type { Request, Response, NextFunction } from "express";
+import { unlink } from "fs/promises";
 
 type RequestWithQuery<T extends z.ZodType> = Request<never, never, never, z.infer<T>>;
 
 type RequestWithParams<T extends z.ZodType> = Request<z.infer<T>, never, never, never>;
+
+const normalizedFiles = <T>(files: T) => {
+  if (Array.isArray(files)) {
+    return files.filter(Boolean);
+  }
+
+  if (files && typeof files === "object") {
+    return Object.entries(files)
+      .flatMap(([_key, file]) => file)
+      .filter(Boolean);
+  }
+
+  return [];
+};
+
+const cleanUpTempMedias = async (req: Request) => {
+  const reqFiles = normalizedFiles(req.files);
+
+  if (req.file) {
+    reqFiles.push(req.file);
+  }
+
+  await Promise.all(reqFiles.map((file) => unlink(file.path)));
+};
 
 export const ZodBodyValidator =
   <T extends z.ZodType>(schema: T) =>
@@ -25,6 +50,8 @@ export const ZodBodyValidator =
       }
 
       const validationError = new ValidationError(message, details);
+
+      cleanUpTempMedias(req).catch(console.error);
 
       next(validationError);
     }
