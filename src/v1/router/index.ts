038e3fe6047ec4222aae4bdbc8eda46fb0fe7f81
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-19 16:33:55 +0800

    feat(router): register tweet endpoint

--- src/v1/router/index.ts
+++ src/v1/router/index.ts
@@ -4,12 +4,16 @@ import { rateLimit } from "express-rate-limit";
 import { rateLimitOptions } from "../configs/rate-limiter.js";
 import { router as authRouter } from "../auth/routes/index.js";
 import { router as userRouter } from "../user/routes/index.js";
+import { router as tweetRouter } from "../tweet/routes/index.js";
 
 const router = Router();
 
 router.use(rateLimit(rateLimitOptions));
 
-router.use("/api/v1/auth", authRouter);
-router.use("/api/v1/users", userRouter);
+const BASE_URL = "/api/v1"
+
+router.use(`${BASE_URL}/auth`, authRouter);
+router.use(`${BASE_URL}/users`, userRouter);
+router.use(`${BASE_URL}/tweets`, tweetRouter)
 
 export { router };
