3c2d928e62952725bda5b15cf34c5f35695429d3
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-20 11:24:12 +0800

    refactor(tweet/schema/reply-tweet): remove replyTo in schema

--- src/v1/tweet/schema/reply-tweet.ts
+++ src/v1/tweet/schema/reply-tweet.ts
@@ -1,14 +1,16 @@
 import z from "zod";
+
 import { MAX_CONTENT_LENGTH } from "../constants/index.js";
+
 import type { TweetMedia } from "../types/tweet.types.js";
 
 export type ReplyTweetDTO = z.infer<typeof replyTweetSchema> & {
+  replyTo: string;
   authorId: string;
   medias: TweetMedia[];
 };
 
 export const replyTweetSchema = z.object({
-  replyTo: z.uuidv7(),
   content: z
     .string()
     .trim()
