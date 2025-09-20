0be69e2996824c555142ca1a23e390028c10ddaf
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-20 12:03:00 +0800

    chore(__mocks__/server): remove logger

--- src/v1/__mocks__/server.ts
+++ src/v1/__mocks__/server.ts
@@ -5,7 +5,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import helmet from "helmet";
-import logger from "morgan";
 import path from "node:path";
 
 import { corsConfig } from "@/configs/cors.js";
@@ -19,7 +18,6 @@ const dirname = import.meta.dirname;
 
 app.use(cors(corsConfig));
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
