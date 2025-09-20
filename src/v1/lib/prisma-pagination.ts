5bbb064fa8bee973c7d4b7285c9b59b08ba2ccea
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-20 13:43:00 +0800

    feat(lib): add toPrismaPagination util for building cursor pagination args

---
+++ src/v1/lib/prisma-pagination.ts
@@ -0,0 +1,46 @@
+export type PaginationDirection = "forward" | "backward" | "none";
+
+export interface PaginationProps {
+  before: string | undefined;
+  after: string | undefined;
+  pageSize: number;
+}
+
+export interface Pagination {
+  skip: 0 | 1;
+  take: number;
+  cursor?: { id: string };
+  direction: PaginationDirection;
+}
+
+export interface PaginationCursor {
+  before: string | undefined;
+  after: string | undefined;
+}
+
+const getDirection = (
+  before: string | undefined,
+  after: string | undefined
+): PaginationDirection => {
+  if (after) return "forward";
+  if (before) return "backward";
+
+  return "none";
+};
+
+export const toPrismaPagination = ({ before, after, pageSize }: PaginationProps) => {
+  const id = before ?? after;
+  const direction = getDirection(before, after);
+
+  const pagination = {
+    direction,
+    skip: direction === "none" ? 0 : 1,
+    take: (direction === "backward" ? -1 : 1) * (pageSize + 1),
+  } as Pagination;
+
+  if (id) {
+    pagination.cursor = { id };
+  }
+
+  return pagination
+};
