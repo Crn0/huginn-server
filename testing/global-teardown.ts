7440b2d5407e2131de5e5d40700a64af7d23dd11
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-23 11:49:32 +0800

    test: clean up temp files

--- testing/global-teardown.ts
+++ testing/global-teardown.ts
@@ -1,19 +1,36 @@
+import path from "node:path";
+import fs from "fs/promises";
 import { prisma } from "@/db/client/prisma.js";
 
 import { seedTestUser } from "./seed.js";
+
+const removeTempFiles = async () => {
+  const tempDir = path.join(import.meta.dirname, "..", "src", "temp");
+
+  const filePaths = await fs.readdir(tempDir);
+
+  await Promise.all(
+    filePaths.map(async (filePath) =>
+      filePath !== ".gitkeep" ? fs.unlink(filePath) : Promise.resolve()
+    )
+  );
+};
 
 export default async () => {
   await seedTestUser();
 
   return async () => {
-    await prisma.$transaction([
-      prisma.userOIDCAccount.deleteMany(),
-      prisma.userProfile.deleteMany(),
-      prisma.tweet.deleteMany(),
-      prisma.user.deleteMany(),
-      prisma.blacklistedToken.deleteMany(),
-      prisma.authProvider.deleteMany(),
-      prisma.media.deleteMany(),
+    await Promise.all([
+      removeTempFiles(),
+      prisma.$transaction([
+        prisma.userOIDCAccount.deleteMany(),
+        prisma.userProfile.deleteMany(),
+        prisma.tweet.deleteMany(),
+        prisma.user.deleteMany(),
+        prisma.blacklistedToken.deleteMany(),
+        prisma.authProvider.deleteMany(),
+        prisma.media.deleteMany(),
+      ]),
     ]);
   };
 };
