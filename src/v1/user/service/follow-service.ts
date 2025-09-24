e0d9d9838f304ee791e9782699fff3c51ae29dad
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-24 11:43:05 +0800

    feat(user/service): add getFollowersCountById, getFollowingCountById and getUserFollowCountById

--- src/v1/user/service/follow-service.ts
+++ src/v1/user/service/follow-service.ts
@@ -5,3 +5,16 @@ export const followUserById = async (userId: string, followId: string) =>
 
 export const unFollowUserById = async (userId: string, unFollowId: string) =>
   followRepository.unFollowUserById(userId, unFollowId);
+
+export const getFollowersCountById = async (userId: string) =>
+  followRepository.getFollowersCountById(userId);
+
+export const getFollowingCountById = async (userId: string) =>
+  followRepository.getFollowingCountById(userId);
+
+export const getUserFollowCountById = async (userId: string) => {
+  const followingCount = await followRepository.getFollowersCountById(userId);
+  const followersCount = await followRepository.getFollowingCountById(userId);
+
+  return Object.freeze({ followingCount, followersCount })
+}
