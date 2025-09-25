975fa5958f0626768cf02d80509924fafaeac620
crno <118383162+Crn0@users.noreply.github.com.>
2025-09-25 15:14:40 +0800

    refactor(types/express): add generics to RequestWithPagination

--- src/v1/types/express.ts
+++ src/v1/types/express.ts
@@ -1,4 +1,4 @@
 import type { Request } from "express";
 import type { PaginationCursor } from "../lib/prisma-pagination.js";
 
-export type RequestWithPagination = Request<unknown, unknown, unknown, PaginationCursor>;
+export type RequestWithPagination<TParams = unknown, TResBody = unknown, TReqBody = unknown> = Request<TParams, TResBody, TReqBody, PaginationCursor>;
