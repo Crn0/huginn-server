c4a70afdbebb64dcb0450cebf8c79e46cefe6fda
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-03 17:18:34 +0800

    feat(user/mapper): transform tweet authors' avatar and banner

--- src/v1/user/mapper/to-user-tweets-response.ts
+++ src/v1/user/mapper/to-user-tweets-response.ts
@@ -2,13 +2,27 @@ import { InternalServerError } from "@/lib/errors/internal-server-error.js";
 import { createDebug } from "@/v1/lib/debug.js";
 import { userTweetsPaginationSchema } from "../schema/user-tweet.js";
 import { transformTweetMedia } from "./transform-tweet-media.js";
+import { transformProfileMedia } from "./transform-profile-media.js";
 
 import type { getTweetsByAuthorIdPagination } from "@/v1/tweet/service/tweet.js";
 
-const debug = createDebug('user:mapper:toUserTweetsResponse')
+const debug = createDebug("user:mapper:toUserTweetsResponse");
 
-export const toUserTweetsResponse = (props: Awaited<ReturnType<typeof getTweetsByAuthorIdPagination>>) => {
-  const tweets = props.tweets.map((tweet) => ({...tweet, media: tweet.media.map(transformTweetMedia) ?? []}))
+export const toUserTweetsResponse = (
+  props: Awaited<ReturnType<typeof getTweetsByAuthorIdPagination>>
+) => {
+  const tweets = props.tweets.map((tweet) => ({
+    ...tweet,
+    author: {
+      ...tweet.author,
+      profile: {
+        ...tweet.author.profile,
+        avatar: transformProfileMedia(tweet.author.profile!.avatar),
+        banner: transformProfileMedia(tweet.author.profile!.banner),
+      },
+    },
+    media: tweet.media.map(transformTweetMedia) ?? [],
+  }));
 
   const parsedData = userTweetsPaginationSchema.safeParse({...props, tweets});
 
