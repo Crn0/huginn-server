33bd3328b03a9545f70d6bd43fdb907b02d0dad5
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-19 18:19:15 +0800

    chore: format code

--- src/v1/user/oidc-account/policy/index.ts
+++ src/v1/user/oidc-account/policy/index.ts
@@ -5,9 +5,7 @@ import type { UserById } from "../../service/user-service.js";
 export const oidcPolicy = {
   delete: (user: UserById) => {
     if (!user.password && user.openIds.length === 1) {
-      throw new ForbiddenError(
-        "You must set a password before unlinking your only auth provider."
-      );
+      throw new ForbiddenError("You must set a password before unlinking your only auth provider.");
     }
   },
 } as const;
