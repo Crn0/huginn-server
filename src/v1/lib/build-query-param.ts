dc0c0e95b6a309fce01305d878492082c7cc781a
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-14 20:48:52 +0800

    lib: add buildQueryParam

---
+++ src/v1/lib/build-query-param.ts
@@ -0,0 +1,13 @@
+export const buildQueryParam = <T extends Record<string, string | number | undefined | null>>(
+  param: T | undefined | null
+) => {
+  const search = new URLSearchParams();
+
+  Object.entries(param ?? {}).forEach(([key, value]) => {
+    if (value) {
+       search.set(key, String(value));
+    }
+  });
+
+  return search.toString();
+};
