566a13e9559901de92a83959b5c8472c2be3e9b0
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-01 14:05:21 +0800

    refactor(schema): change username min length to 3

--- src/v1/user/schema/patch-username.ts
+++ src/v1/user/schema/patch-username.ts
@@ -5,8 +5,8 @@ import { usernameRegex } from "@/v1/lib/user-schema.js";
 export const patchUsernameSchema = z.object({
   username: z
     .string()
-    .min(4, "Username must be at least 4 characters and no more than 15 characters long")
-    .max(15, "Username must be at least 4 characters and no more than 15 characters long")
+    .min(3, "Username must be at least 3 characters and no more than 15 characters long")
+    .max(15, "Username must be at least 3 characters and no more than 15 characters long")
     .refine((val) => usernameRegex.test(val), {
       message:
         "Username can only contain letters (A-Z, a-z), numbers (0-9), and the characters: _ , .",
