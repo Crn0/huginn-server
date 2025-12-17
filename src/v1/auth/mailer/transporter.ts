2e427f26708a61f88c5296545d1ec2787214fd0f
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-18 01:29:24 +0800

    feat(transporter): add host and port

--- src/v1/auth/mailer/transporter.ts
+++ src/v1/auth/mailer/transporter.ts
@@ -4,6 +4,8 @@ import { env } from "@/configs/env.js";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
   secure: true,
   auth: {
     user: env.GOOGLE_GMAIL,
