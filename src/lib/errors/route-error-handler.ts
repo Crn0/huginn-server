0ba1be83c944fd97440b889508395cdc478175dd
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-18 00:59:06 +0800

    log error

--- src/lib/errors/route-error-handler.ts
+++ src/lib/errors/route-error-handler.ts
@@ -32,6 +32,7 @@ export const routeErrorHandler = (
   res: Response,
   _next: NextFunction
 ) => {
+  console.log(error)
   switch (error.kind) {
     case "AUTHENTICATION_ERROR":
     case "BAD_REQUEST_ERROR":
