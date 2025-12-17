ad0cbab84dac15dc1de4ef20567a01e7bbfd0d9a
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-18 00:05:50 +0800

    fix(auth): await sendMailPasswordReset

--- src/v1/auth/api/request-reset-password.ts
+++ src/v1/auth/api/request-reset-password.ts
@@ -16,7 +16,7 @@ export const requestResetPassword = async (req: Request, res: Response) => {
   if (user) {
     const token = generateActionToken(user.id, { username: user.username }, ACCESS_TOKEN_EXP);
 
-    sendMailPasswordReset(user, token);
+    await sendMailPasswordReset(user, token);
   }
 
   return res.sendStatus(NO_CONTENT);
